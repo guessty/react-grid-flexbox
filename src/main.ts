20e0df52e5fd9022b44abb8715427170219bff56
Christian <christian.guest@outlook.com>
2018-08-20 03:24:50 +0100

    added DOM types for grid and flex children

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+import 'react'
 import Grid from './components/Grid/index'
 import GridItem from './components/Grid/GridItem/index'
 import Flex from './components/Flex/index'
 import FlexItem from './components/Flex/FlexItem/index'
+
+declare module "react" {
+  interface HTMLAttributes<T> extends React.DOMAttributes<T> {
+    _gridArea?: string | object
+    _flexBasis?: string | object
+    _flexGrow?: boolean
+    _flexScroll?: boolean
+    _flexReset?: boolean
+  }
+}
 
 export {
   Grid,
